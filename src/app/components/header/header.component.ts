fdd9af9e6e258d93734338763a8aeefea9d44841
ShaneDsouza4 <shane.json96@gmail.com>
2025-05-26 19:14:22 -0400

    Installed Tailwind

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,7 +4,7 @@ import { Component, signal } from '@angular/core';
   selector: 'app-header',
   imports: [],
   template: `
-    <div class="header">
+    <div class="bg-slate-100 px-4 py-3 shadow-md">
       {{title()}}
     </div>
   `,
